07deaf9b2f759441e0b1ddcadfdfdcae6a6cb614
daniil.gerasimov <daniil.gerasimov@travelline.ru>
2025-04-24 18:13:37 +0300

    improve

--- LifetimeDemo/Controllers/LifetimeController.cs
+++ LifetimeDemo/Controllers/LifetimeController.cs
@@ -10,25 +10,50 @@ namespace LifetimeDemo.Controllers
         private readonly ISingletonService _singletonService;
         private readonly IScopedService _scopedService;
         private readonly ITransientService _transientService;
+        private readonly IServiceProvider _serviceProvider;
 
         public LifetimeController(
             ISingletonService singletonService,
             IScopedService scopedService,
-            ITransientService transientService)
+            ITransientService transientService,
+            IServiceProvider serviceProvider)
         {
             _singletonService = singletonService;
             _scopedService = scopedService;
             _transientService = transientService;
+            _serviceProvider = serviceProvider;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("singleton")]
+        public IActionResult GetSingleton()
         {
+            var singleton2 = (ISingletonService)_serviceProvider.GetService(typeof(ISingletonService));
             return Ok(new
             {
-                Singleton = _singletonService.Id,
-                Scoped = _scopedService.Id,
-                Transient = _transientService.Id
+                Singleton_1 = _singletonService.Id,
+                Singleton_2 = singleton2?.Id
+            });
+        }
+
+        [HttpGet("scoped")]
+        public IActionResult GetScoped()
+        {
+            var scoped2 = (IScopedService)_serviceProvider.GetService(typeof(IScopedService));
+            return Ok(new
+            {
+                Scoped_1 = _scopedService.Id,
+                Scoped_2 = scoped2?.Id
+            });
+        }
+
+        [HttpGet("transient")]
+        public IActionResult GetTransient()
+        {
+            var transient2 = (ITransientService)_serviceProvider.GetService(typeof(ITransientService));
+            return Ok(new
+            {
+                Transient_1 = _transientService.Id,
+                Transient_2 = transient2?.Id
             });
         }
     }
